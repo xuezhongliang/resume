29752b6273a1485870fd3a17736454b6edc730bf
xuezhongliang <zhongliang.xue@bestulearn.com>
2022-02-09 00:39:01 +0800

    傻逼傻逼项目项目

--- src/component/Resume/main.jsx
+++ src/component/Resume/main.jsx
@@ -11,9 +11,7 @@ var Resume = React.createClass({
                 </header>
                 <BasicInfo basicinfo={this.props.resume.basicinfo}/>
                 <Social social={this.props.resume.social}/>
-                <ExperienceList type="获奖经历" experiences={this.props.resume.Awards}/>
-                <ExperienceList type="社团/组织" experiences={this.props.resume.socialExperiences}/>
-                <ExperienceList type="实习经历" experiences={this.props.resume.InternExperiences}/>
+                <ExperienceList type="工作经历" experiences={this.props.resume.socialExperiences}/>
                 <ExperienceList type="项目经验" experiences={this.props.resume.ProjectExperience}/>
             </div>
         )
